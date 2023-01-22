6f8b85c45ac6bfc360cf27365138ba621040ad7e
keneversley <krazorfbx@gmail.com>
2023-01-22 09:57:04 -0800

    added calculator

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -7,19 +7,44 @@ public class LoanCalculator {
     /**
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     *
+     * @param ??? return methods that can be painted on the screen
+     * @return ??? returns value from a method
      */
-    static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
-    }
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int amount = scanner.nextInt();
-            System.out.println(getRemainingAmountIn3Months(amount));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the amount of your loan: ");
+        int amount = sc.nextInt();
+        int l = amount;
+        int percent;
+        int payment;
+
+        System.out.println("You need to pay 10% of the remaining amount each month.");
+        System.out.println("");
+
+        for (int i = 0 ; i < 1 ; i++){
+            percent = l * 10 /100;
+            payment = l - percent;
+            l = payment;
         }
+        System.out.println("The remaining amount after 1 month is " + l);
+
+        for (int i = 0 ; i < 1 ; i++) {
+            percent = l * 10 /100;
+            payment = l - percent;
+            l = payment;
+        }
+        System.out.println("The remaining amount after 2 months is " + l);
+
+        for (int i = 0 ; i < 1; i++) {
+            percent = l * 10 /100;
+            payment = l - percent;
+            l = payment;
+        }
+        System.out.println("The remaining amount after 3 months is " + l);
     }
 }
+
